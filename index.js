908b9b8f50562bbad361d20e4dc91aeccc2e11e4
tuuuu878 <strenchi43pun0suy@hotmail.com>
2024-05-15 14:55:37 +0700

    hello

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-function isValidSudoku(board) {
-  for (let i = 0; i < 9; i++) {
-    const row = new Set();
-    const col = new Set();
-    const box = new Set();
-    for (let j = 0; j < 9; j++) {
-      const rowVal = board[i][j];
-      const colVal = board[j][i];
-      const boxVal =
-        board[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)];
-      if (rowVal !== ".") {
-        if (row.has(rowVal)) return false;
-        row.add(rowVal);
-      }
-      if (colVal !== ".") {
-        if (col.has(colVal)) return false;
-        col.add(colVal);
-      }
-      if (boxVal !== ".") {
-        if (box.has(boxVal)) return false;
-        box.add(boxVal);
+function wordBreak(s, wordDict) {
+  const wordSet = new Set(wordDict);
+  const dp = new Array(s.length + 1).fill(false);
+  dp[0] = true;
+  for (let end = 1; end <= s.length; end++) {
+    for (let start = 0; start < end; start++) {
+      if (dp[start] && wordSet.has(s.substring(start, end))) {
+        dp[end] = true;
+        break;
       }
     }
   }
-  return true;
+  return dp[s.length];
 }
